2a88a78d2f2701dc7282d0d504a5b607ea0afa7e
Binal Patel <147269633+Binalpatel1012@users.noreply.github.com>
2024-04-17 00:28:17 +0000

    In Program.cs, setup dependency injection to inject IFeatureManager

--- GeometryConsole/Program.cs
+++ GeometryConsole/Program.cs
@@ -1,2 +1,39 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Configuration;
+using Microsoft.FeatureManagement;
+
+class Program
+{
+    static async Task Main(string[] args)
+    {
+        
+        var featureManagement = new Dictionary<string, string>
+        {
+            { "FeatureManagement:Square", "true"},
+            { "FeatureManagement:Rectangle", "false"},
+            { "FeatureManagement:Triangle", "true"}
+        };
+
+        IConfigurationRoot configuration = new ConfigurationBuilder()
+            .AddInMemoryCollection(featureManagement)
+            .Build();
+
+        
+        var services = new ServiceCollection();
+        services.AddFeatureManagement(configuration);
+        var serviceProvider = services.BuildServiceProvider();
+
+        
+        var featureManager = serviceProvider.GetRequiredService<IFeatureManagerSnapshot>();
+
+       
+        if (await featureManager.IsEnabledAsync("Square"))
+        {
+            Console.WriteLine("Feature Square is enabled");
+        }
+        else
+        {
+            Console.WriteLine("Feature Square is not available");
+        }
+    }
+}
